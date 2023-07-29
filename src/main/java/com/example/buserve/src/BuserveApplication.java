4a4e98ab378568245245989eed1e23f7ce52873e
hsuush <dlhyes703@naver.com>
2023-07-29 23:52:41 +0900

    feat : 실행파일 생성

---
+++ src/main/java/com/example/buserve/src/BuserveApplication.java
@@ -0,0 +1,12 @@
+package com.example.buserve.src;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuserveApplication {
+    public static void main(String[] args){
+        SpringApplication.run(BuserveApplication.class, args);
+    }
+
+}
