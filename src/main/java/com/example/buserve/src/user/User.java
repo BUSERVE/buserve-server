b33abd834a113d9c2e386f6194f778ffe8330ab3
hsuush <dlhyes703@naver.com>
2023-08-06 16:35:27 +0900

    feat: user 관련 file 수정

--- src/main/java/com/example/buserve/src/user/User.java
+++ src/main/java/com/example/buserve/src/user/User.java
@@ -1,8 +1,18 @@
 package com.example.buserve.src.user;
 
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Collection;
+import java.util.Map;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -14,33 +24,42 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id")
-    private Long id;
+    @Column(name = "USER_SEQ")
+    private Long userSeq;
 
-    @Column(nullable = false)
+    @Column(name ="EMAIL", nullable = false, unique = true)
     private String email; // 이메일
 
-    @Column(nullable = false)
+    @Column(name = "NICKNAME",nullable = false)
     private String nickname; // 닉네임
 
+    @Column(name = "PROFILE_IMAGE_URL")
     private String imageUrl; // 프로필 이미지
 
+    @Column(name = "ROLE")
     @Enumerated(EnumType.STRING)
     private Role role;
 
+    @Column(name = "SOCIALTYPE")
     @Enumerated(EnumType.STRING)
     private SocialType socialType; // KAKAO, APPLE, GOOGLE
 
-    private String socialId; // 로그인한 소셜 타입의 식별자 값 (일반 로그인인 경우 null)
-    private String refreshToken; // 리프레시 토큰
+    public User(@NotNull String email,
+                @NotNull String nickname,
+                @NotNull String imageUrl,
+                @NotNull Role role,
+                @NotNull SocialType socialType)
+    {
+        this.email = email;
+        this.nickname = nickname;
+        this.imageUrl = imageUrl;
+        this.role = role;
+        this.socialType = socialType;
+    }
 
     // 유저 권한 설정 메소드
     public void authorizeUser() {
         this.role = Role.USER;
     }
 
-
-    public void updateRefreshToken(String updateRefreshToken) {
-        this.refreshToken = updateRefreshToken;
-    }
 }
