5d13a5703f1cd293b3bd6d1a8ce0d463ef40c427
JJongmen <pjy90123@gmail.com>
2023-08-24 03:31:24 +0900

    feat: 인천 광역버스 데이터 추가

--- src/main/java/com/example/buserve/src/configure/DatabaseSeeder.java
+++ src/main/java/com/example/buserve/src/configure/DatabaseSeeder.java
@@ -94,6 +94,8 @@ public class DatabaseSeeder {
                 .build();
         chargingMethodRepository.save(method3);
     }
+
+    /*
     private void seedBusStopsAndRoutesAndReservations(
             BusRepository busRepository,
             RouteRepository routeRepository,
@@ -177,5 +179,96 @@ public class DatabaseSeeder {
 
         Reservation reservation2 = new Reservation(user1, bus2.getSeats().get(10), stop2, LocalDate.now().plusDays(1).atTime(7, 30));
         reservationRepository.save(reservation2);
+    }
+     */
+    private void seedBusStopsAndRoutesAndReservations(
+            BusRepository busRepository,
+            RouteRepository routeRepository,
+            RouteService routeService,
+            StopRepository stopRepository,
+            RouteStopRepository routeStopRepository,
+            SeatRepository seatRepository,
+            UserRepository userRepository,
+            ReservationRepository reservationRepository) throws IOException, ParseException {
+
+        String key = "lF8UEJMTnm7SpZKEcgBRzazgp0JNAxAwLEu9H%2BG844NuHoC4DZS8qbdDNpM1WoBTq1jimtK%2BW2P6N4kksiuwBQ%3D%3D";
+        String cityCode = "23";
+        String endPoint = "http://apis.data.go.kr/1613000/";
+
+        for (int pageNo = 1; pageNo <= 4; pageNo++) {
+            String service = "BusRouteInfoInqireService/";
+            String details = "getRouteNoList";
+            URL url = new URL(endPoint + service + details + "?serviceKey=" + key
+                    + "&cityCode=" + cityCode + "&pageNo=" + pageNo + "&numOfRows=100" + "&_type=json");
+
+            BufferedReader bf = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
+            String result = bf.readLine();
+            JSONParser jsonParser = new JSONParser();
+            JSONObject jsonObject = (JSONObject) jsonParser.parse(result);
+            JSONObject parseItems = (JSONObject) ((JSONObject) ((JSONObject) jsonObject.get("response")).get("body")).get("items");
+            JSONArray array = (JSONArray) parseItems.get("item");
+
+            for (int i = 0; i < array.size(); i++) {
+                jsonObject = (JSONObject) array.get(i);
+//                System.out.println(jsonObject);
+                String routeId = jsonObject.get("routeid").toString();      // 노선 ID
+                String routeName = jsonObject.get("routeno").toString();      // 노선 이름
+                String routeType = jsonObject.get("routetp").toString();      // 노선 유형
+
+                if (routeType.equals("광역버스")) {
+                    final Route route = new Route(routeId, routeName);
+                    routeRepository.save(route);
+
+                    // Creating Stops
+                    creatingStopsAndRouteStops(stopRepository, routeStopRepository, endPoint, key, cityCode, route);
+
+                    // Creating Bus
+                    for (int j = 0; j < 3; j++) {
+                        Bus bus = new Bus(20, LocalTime.of(6, 0).plusMinutes(30 * j), route);
+                        busRepository.save(bus);
+                        seatRepository.saveAll(bus.getSeats());
+                    }
+                }
+            }
+        }
+    }
+
+    private static void creatingStopsAndRouteStops(final StopRepository stopRepository, final RouteStopRepository routeStopRepository, final String endPoint, final String key, final String cityCode, final Route route) throws IOException, ParseException {
+        // Creating Stops
+        // List<String> routeIds = routeService.getAllRouteId();
+        String service = "BusRouteInfoInqireService/";
+        String details = "getRouteAcctoThrghSttnList";
+        URL url = new URL(endPoint + service + details + "?serviceKey=" + key
+                + "&cityCode=" + cityCode + "&numOfRows=100" + "&routeId=" + route.getId() + "&_type=json");
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
+        String result = bf.readLine();
+        JSONParser jsonParser = new JSONParser();
+        JSONObject jsonObject = (JSONObject) jsonParser.parse(result);
+        JSONObject parseItems = (JSONObject) ((JSONObject) ((JSONObject) jsonObject.get("response")).get("body")).get("items");
+        JSONArray array = (JSONArray) parseItems.get("item");
+
+        for (int j = 0; j < array.size(); j++) {
+            jsonObject = (JSONObject) array.get(j);
+            String routeid = jsonObject.get("routeid").toString();      // 노선 ID
+            String nodeid = jsonObject.get("nodeid").toString();        // 정류소 ID
+            String nodeno = jsonObject.get("nodeno").toString();        // 정류소 번호
+            String nodenm = jsonObject.get("nodenm").toString();        // 정류소 명
+            String gpslati = jsonObject.get("gpslati").toString();      // 정류소 위도(Y좌표)
+            String gpslong = jsonObject.get("gpslong").toString();      // 정류소 경도(X좌표)
+            String nodeord = jsonObject.get("nodeord").toString();      // 정류소 순번
+            Direction updowncd;                                         // 상하행 [0: 상행 1: 하행]
+            if (jsonObject.get("updowncd").toString().equals("0")) { updowncd = Direction.UPWARD; }
+            else { updowncd = Direction.DOWNWARD; }
+
+            Stop stop = new Stop(nodeid, nodenm, nodeno, gpslati, gpslong);
+            stopRepository.save(stop);
+
+            // Creating RouteStop
+            int minutesToAdd = (Integer.parseInt(nodeord) - 1) * 5;
+            LocalTime time = LocalTime.of(0, 0).plusMinutes(minutesToAdd);
+            RouteStop routeStop = new RouteStop(route, stop, Integer.parseInt(nodeord), time, updowncd);
+            routeStopRepository.save(routeStop);
+        }
     }
 }
