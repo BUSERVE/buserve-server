912a21d7dfc3edfcd3cfe037618d47ab966258ab
JJongmen <pjy90123@gmail.com>
2023-08-23 14:38:05 +0900

    fix: 병합 후 작동안되던 버그 수정

--- src/main/java/com/example/buserve/src/configure/WebSecurityConfig.java
+++ src/main/java/com/example/buserve/src/configure/WebSecurityConfig.java
@@ -1,12 +1,14 @@
 package com.example.buserve.src.configure;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
+@Order(1)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
